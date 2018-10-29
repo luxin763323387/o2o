68dea8a6dc82019a6cd4e2248515f0524daa379a
StevenLu <luxin763323387@163.com>
2018-10-29 12:26:05 +0800

    商店列表排序和商品排版问题

--- src/main/webapp/resources/js/shop/productcategorymanagement.js
+++ src/main/webapp/resources/js/shop/productcategorymanagement.js
@@ -19,7 +19,7 @@ $(function () {
                                 +'<div class="row row-product-category now">'
                                 +'<div class="col-33 product-category-name">'
                                 +item.productCategoryName
-                                +'</div>>'
+                                +'</div>'
                                 +'<div class="col-33">'
                                 + item.priority
                                 +'</div>'
